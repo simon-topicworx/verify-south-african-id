61f69df659c7b2364f39e6b74a8e4434acd81b6e
Yens Loff <yens.loff@gmail.com>
2019-06-11 20:32:56 +0200

    localstorage feature

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 This form takes a valid 13 digit input<br/>
 And outputs the following - `ID : Female/Male : Citezen/Permanent Residence : Date of Birth(d-mm-yyyy)`<br/>
 <br/>
-To see this code in action. [Click here...](https://yenszaf.github.io/verify-south-african-id/ "Verify South African ID")
+To see this code in action. [Click here...](https://yenszaf.github.io/verify-south-african-id/ "Verify South African ID")<br/>
+<br/>
+Credit to [Tania Rascia](https://www.taniarascia.com/how-to-use-local-storage-with-javascript/ "Tania Rascia") for all code related to `localStorage`<br/>
+for locally storing storing submissions on clients browser, with an option to clear/delete all stored data. 
--- main.js
+++ main.js
@@ -1,8 +1,14 @@
-const idInput = document.querySelector('#verify_id');
+let idInput = document.querySelector('#verify_id');
 const myForm = document.querySelector('#my-form');
 const msg = document.querySelector('.msg');
 const msgId = document.querySelector('.invalid-id');
 const userList = document.querySelector('#users');
+const clearButton = document.querySelector('#clear');
+
+// setting variables for localStorage. localStorage related code credit: https://www.taniarascia.com/how-to-use-local-storage-with-javascript/
+let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+localStorage.setItem('items', JSON.stringify(itemsArray));
+const data = JSON.parse(localStorage.getItem('items'));
 
 idInput.addEventListener('input', checkId);
 myForm.addEventListener('submit', onSubmit);
@@ -10,7 +16,7 @@ myForm.addEventListener('submit', onSubmit);
 // Event listener for input in ID text field. Displays green when true.
 function checkId(e) {
     e.preventDefault();
-    const ID = verify_id(idInput.value);
+    let ID = verify_id(idInput.value);
     if (ID.Valid === true){
         idInput.style.borderColor = 'green';
         msgId.innerHTML = '';
@@ -57,10 +63,18 @@ function verify_id(num) {
     return {Valid:num[num.length - 1] === String(step6),Gender:gender,Citizen:citizenship,BirthDate:DoB};
 }
 
+
+// Function to create a li element.
+const liMaker = text => {
+  const li = document.createElement('li')
+  li.textContent = text
+  userList.appendChild(li)
+}
+
 // Event listener on submit. checks id valid then returns values as li element.
 function onSubmit(e) {
     e.preventDefault();
-    const ID = verify_id(idInput.value);
+    let ID = verify_id(idInput.value);
     if (!ID.Valid) {
         msg.innerHTML = 'Please enter all fields';
         setTimeout(function () {
@@ -68,10 +82,31 @@ function onSubmit(e) {
         },3000);
         //setTimeout(() => msg.innerHTML = '', 3000); //uses pointer function
     } else {
-        const li = document.createElement('li');
-        li.appendChild(document.createTextNode(`${idInput.value} : ${ID.Gender} : ${ID.Citizen} : ${ID.BirthDate}`));
-        userList.appendChild(li);
+        //**OLD CODE**
+        //const li = document.createElement('li');
+        //li.appendChild(document.createTextNode(`${idInput.value} : ${ID.Gender} : ${ID.Citizen} : ${ID.BirthDate}`));
+        //userList.appendChild(li);
         //Clear fields
+        //idInput.value = '';
+        
+        //Adds data to an array and creates a li item.
+        itemsArray.push(`${idInput.value} : ${ID.Gender} : ${ID.Citizen} : ${ID.BirthDate}`);
+        localStorage.setItem('items', JSON.stringify(itemsArray));
+        liMaker(`${idInput.value} : ${ID.Gender} : ${ID.Citizen} : ${ID.BirthDate}`);
+        ID = '';
         idInput.value = '';
     }
 }
+
+// Creates li for each submit
+data.forEach(item => {
+  liMaker(item)
+})
+
+// Clears localStorage of all submissions
+clearButton.addEventListener('click', function() {
+  localStorage.clear()
+  while (userList.firstChild) {
+    userList.removeChild(userList.firstChild)
+  }
+})
